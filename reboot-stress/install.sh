37728f393843e6729965e394c277bfcf06cea8ab
Kirt Hsieh <kirt_hsieh@asus.com>
2016-10-05 16:35:54 +0800

    reboot-stress: Update install script

--- reboot-stress/install.sh
+++ reboot-stress/install.sh
@@ -2,7 +2,23 @@
 
 LOCAL_PATH=$(pwd)
 
-chmod a+x $LOCAL_PATH/reboot-stress
-sudo cp $LOCAL_PATH/reboot-stress /usr/bin/
-sudo cp reboot-stress.desktop $HOME/.config/autostart
-echo "Install Complete!!"
+PARA=$1
+
+if [ $# -eq 1 ]; then
+    PARA=$1
+    if [ $PARA = "-r" ]; then
+        sudo rm /usr/bin/reboot-stress
+        sudo rm $HOME/.config/autostart/reboot-stress.desktop
+        sudo systemctl enable hciuart
+        echo "Remove Complete!!"
+        sudo reboot
+    fi
+fi
+if [ $# -eq 0 ]; then
+    chmod a+x $LOCAL_PATH/reboot-stress
+    sudo cp $LOCAL_PATH/reboot-stress /usr/bin/
+    sudo cp reboot-stress.desktop $HOME/.config/autostart
+    sudo systemctl disable hciuart
+    echo "Install Complete!!"
+    sudo reboot
+fi
